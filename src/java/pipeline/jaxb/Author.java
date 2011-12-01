ccd24128df6794cb98800216d1e5fd1402339ded
Weilu Jia <optix2000@gmail.com>
2011-11-30 22:13:18 -0800

    Update JAXB to schema

--- src/java/pipeline/jaxb/Author.java
+++ src/java/pipeline/jaxb/Author.java
@@ -15,19 +15,17 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for metadataName complex type.
+ * <p>Java class for author complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="metadataName">
+ * &lt;complexType name="author">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;choice>
- *         &lt;element name="stringExtract" type="{}stringExtract"/>
- *       &lt;/choice>
- *       &lt;attribute name="nameOption" type="{http://www.w3.org/2001/XMLSchema}string" />
- *       &lt;attribute name="nameValue" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="fullName" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="email" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="website" type="{http://www.w3.org/2001/XMLSchema}string" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -36,87 +34,86 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "metadataName", propOrder = {
-    "stringExtract"
-})
-public class MetadataName {
+@XmlType(name = "author")
+public class Author {
 
-    protected StringExtract stringExtract;
-    @XmlAttribute(name = "nameOption")
-    protected String nameOption;
-    @XmlAttribute(name = "nameValue")
-    protected String nameValue;
-
-    /**
-     * Gets the value of the stringExtract property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link StringExtract }
-     *     
-     */
-    public StringExtract getStringExtract() {
-        return stringExtract;
-    }
+    @XmlAttribute(name = "fullName")
+    protected String fullName;
+    @XmlAttribute(name = "email")
+    protected String email;
+    @XmlAttribute(name = "website")
+    protected String website;
 
     /**
-     * Sets the value of the stringExtract property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link StringExtract }
-     *     
-     */
-    public void setStringExtract(StringExtract value) {
-        this.stringExtract = value;
-    }
-
-    /**
-     * Gets the value of the nameOption property.
+     * Gets the value of the fullName property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getNameOption() {
-        return nameOption;
+    public String getFullName() {
+        return fullName;
     }
 
     /**
-     * Sets the value of the nameOption property.
+     * Sets the value of the fullName property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setNameOption(String value) {
-        this.nameOption = value;
+    public void setFullName(String value) {
+        this.fullName = value;
     }
 
     /**
-     * Gets the value of the nameValue property.
+     * Gets the value of the email property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getNameValue() {
-        return nameValue;
+    public String getEmail() {
+        return email;
     }
 
     /**
-     * Sets the value of the nameValue property.
+     * Sets the value of the email property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setNameValue(String value) {
-        this.nameValue = value;
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Gets the value of the website property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * Sets the value of the website property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setWebsite(String value) {
+        this.website = value;
     }
 
 }
